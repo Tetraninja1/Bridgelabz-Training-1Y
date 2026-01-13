3f81159b232c02724a7074a18b1a387fb17d5c3a
shreyash <shreyashkatiyaer2020@gmail.com>
2026-01-13 22:36:07 +0530

    Problem_3

---
+++ Practice problems/Problem_3.java
@@ -0,0 +1,7 @@
+public class Problem_3{
+	public static void main(String[] args){
+	double i = 10.8;
+	double m = i * 0.621371;
+	System.out.println("The distance 10.8 km in miles is " + m);
+	}
+	}
