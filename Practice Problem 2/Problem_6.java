1b6c2ad4a68068f19815530d08071bb79be53c53
shreyash <shreyashkatiyaer2020@gmail.com>
2026-01-14 01:35:25 +0530

    Problem_6

---
+++ Practice Problem 2/Problem_6.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Problem_6{
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter value of a: ");
+        int a = in.nextInt();
+
+        System.out.print("Enter value of b: ");
+        int b = in.nextInt();
+
+        System.out.print("Enter value of c: ");
+        int c = in.nextInt();        
+        int result1 = a + b * c;
+        int result2 = a * b + c;
+        int result3 = c + a / b;
+        int result4 = a % b + c;
+
+
+        System.out.println("The results of Int Operations are:");
+        System.out.println(result1 + ", " + result2 + ", " + result3 + ", and " + result4);
+		}
+}
