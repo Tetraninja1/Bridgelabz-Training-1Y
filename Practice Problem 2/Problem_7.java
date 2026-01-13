815991454daa7c7ae2eb447b7b79aadc878d9182
shreyash <shreyashkatiyaer2020@gmail.com>
2026-01-14 01:40:19 +0530

    Problem_7

---
+++ Practice Problem 2/Problem_7.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Problem_7{
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter a: ");
+        double a = in.nextDouble();
+        System.out.print("Enter b: ");
+        double b = in.nextDouble();
+        System.out.print("Enter c: ");
+        double c = in.nextDouble();
+        double result1 = a + b * c;
+        double result2 = a * b + c;
+        double result3 = c + a / b;
+        double result4 = a % b + c;
+        System.out.println("The results of Double Operations are:");
+        System.out.println(result1 + ", " + result2 + ", " + result3 + ", and " + result4);
+
+    }
+}
