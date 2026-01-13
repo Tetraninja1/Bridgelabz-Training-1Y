19fd41964de8ad705abebd3cf70d36f97a0b5eed
shreyash <shreyashkatiyaer2020@gmail.com>
2026-01-13 22:46:51 +0530

    Problem_4

---
+++ Practice problems/Problem_4.java
@@ -0,0 +1,11 @@
+public class Problem_4{
+	public static void main(String[] args){
+	float cp = 129;
+	float sp = 191;
+	float profit = sp - cp;
+	float Percent = (profit / cp) * 100;
+	System.out.println("The Cost Price is 129 INR and Selling Price is 191 INR");
+	System.out.println("The Profit is INR "+ profit);
+	System.out.println("and the Profit Percentage is " + Percent);
+	}
+	}
