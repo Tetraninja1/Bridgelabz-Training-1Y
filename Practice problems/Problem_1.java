52250799107a5d74deb313602700f3eaf341bc38
shreyash <shreyashkatiyaer2020@gmail.com>
2026-01-13 22:25:47 +0530

    Problem_1

---
+++ Practice problems/Problem_1.java
@@ -0,0 +1,9 @@
+public class Problem_1{
+	public static void main(String[] args){
+	String name = "Harry's";
+	int born = 2000;
+	int current = 2024;
+	int age = current - born;
+	System.out.println("Harry's age is :" + age);
+	}
+	}
