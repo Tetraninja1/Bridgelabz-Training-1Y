fa2f8ba1e23d13526d9d693c15114a2c08a83893
shreyash <shreyashkatiyaer2020@gmail.com>
2026-01-13 22:32:29 +0530

    Problem_2

---
+++ Practice problems/Problem_2.java
@@ -0,0 +1,9 @@
+public class Problem_2{
+	public static void main(String[] args){
+	int maths = 94;
+	int chem = 96;
+	int phy = 95;
+	int avg = (maths + chem + phy)/3;
+	System.out.println("Sam's average marks in PCM is :" + avg);
+	}
+	}
