1f0af9aea149155a6f1b5559773ebaaf0f3a1900
shreyash <shreyashkatiyaer2020@gmail.com>
2026-01-22 15:07:10 +0530

    Scanner class

--- Loop.java
+++ Loop.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 public class Loop{
     public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		
 		for(int i=0;i<10;i++){
 			System.out.println("i : "+ i);
 		}
