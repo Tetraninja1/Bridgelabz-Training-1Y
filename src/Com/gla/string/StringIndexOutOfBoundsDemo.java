ad5ccd0607715e5b0ae27223e6e48a7686cd84ef
Shreyash <shreyashkatiyar2020@gmail.com>
2026-02-10 22:56:45 +0530

    [Shreyash] Added StringIndexOutOfBoundsDemo

---
+++ src/Com/gla/string/StringIndexOutOfBoundsDemo.java
@@ -0,0 +1,35 @@
+package Com.gla.string;
+import java.util.Scanner;
+
+public class StringIndexOutOfBoundsDemo {
+
+        public static void generateException(String text) {
+            System.out.println(text.charAt(text.length()));
+        }
+
+        public static void handleException(String text) {
+
+            try {
+                System.out.println(text.charAt(text.length()));
+            } catch (StringIndexOutOfBoundsException e) {
+                System.out.println("StringIndexOutOfBoundsException caught!");
+                System.out.println("Invalid index access attempted.");
+            }
+        }
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter a string: ");
+            String text = sc.next();
+
+            generateException(text);
+
+            handleException(text);
+
+            System.out.println("Program continues after handling the exception.");
+
+            sc.close();
+        }
+    }
