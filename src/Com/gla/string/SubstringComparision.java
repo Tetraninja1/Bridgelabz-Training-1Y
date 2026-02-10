6e07292c98184622e52fc989e70886910e3ac473
Shreyash <shreyashkatiyar2020@gmail.com>
2026-02-10 22:35:41 +0530

    [Shreyash] Added SubstringComparision

--- src/Com/gla/string/SubstringComparision.java
+++ src/Com/gla/string/SubstringComparision.java
@@ -1,4 +1,55 @@
 package Com.gla.string;
+import java.util.Scanner;
 
 public class SubstringComparision {
-}
+            public static String substringUsingCharAt(String text, int start, int end) {
+                String result = "";
+
+                for (int i = start; i < end; i++) {
+                    result += text.charAt(i);
+                }
+
+                return result;
+            }
+
+            public static boolean compareUsingCharAt(String s1, String s2) {
+
+                if (s1.length() != s2.length()) {
+                    return false;
+                }
+
+                for (int i = 0; i < s1.length(); i++) {
+                    if (s1.charAt(i) != s2.charAt(i)) {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            public static void main(String[] args) {
+
+                Scanner sc = new Scanner(System.in);
+
+                System.out.print("Enter a string: ");
+                String text = sc.next();
+
+                System.out.print("Enter start index: ");
+                int start = sc.nextInt();
+
+                System.out.print("Enter end index: ");
+                int end = sc.nextInt();
+
+                String manualSubstring = substringUsingCharAt(text, start, end);
+
+                String builtInSubstring = text.substring(start, end);
+
+                boolean areEqual = compareUsingCharAt(manualSubstring, builtInSubstring);
+
+                System.out.println("Substring using charAt(): " + manualSubstring);
+                System.out.println("Substring using substring(): " + builtInSubstring);
+                System.out.println("Are both substrings equal? " + areEqual);
+
+                sc.close();
+            }
+        }
